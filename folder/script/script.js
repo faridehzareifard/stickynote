322b83f30bf2b6a5a1abc78c76c748808b7da40a
faridehzareifard <85130931+faridehzareifard@users.noreply.github.com>
2022-01-29 23:17:50 +0330

    add js
    use js for show and hide plus page

--- folder/script/script.js
+++ folder/script/script.js
@@ -1,2 +1,15 @@
-let a = document.getElementById("plus")
-console.log(a)
+function changetoshow() {
+    let click = document.getElementById("click")
+    let pluspage = document.getElementById("plus-page");
+    console.log(pluspage)
+
+    pluspage.classList.add("show")
+
+
+}
+
+function changetohide() {
+    let pluspage = document.getElementById("plus-page");
+    pluspage.classList.remove("show")
+
+}
